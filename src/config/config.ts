315ad939a450bdabfa1aeb6ee732708aa352e75a
evandrocaslini <evandro.caslini2001@gmail.com>
2024-10-18 23:52:26 -0300

    control flow

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,3 +6,6 @@ export const RAW_FILE_PATH = process.env.RAW_FILE_PATH || '';
 export const PROCESSED_FILE_BASE_PATH = process.env.PROCESSED_FILE_BASE_PATH || '';
 export const REST_API_BASE_URL = process.env.REST_API_BASE_URL || 'http://localhost:3000';
 export const GRAPHQL_API_BASE_URL = process.env.GRAPHQL_API_BASE_URL || 'http://localhost:3000/graphql';
+export const SHOULD_TRANSFORM = process.env.SHOULD_TRANSFORM || true;
+export const SHOULD_POPULATE = process.env.SHOULD_POPULATE || true;
+export const SHOULD_TEST = process.env.SHOULD_TEST || true;
--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,30 @@ import {
   testHandler,
 } from './scripts';
 
+import {
+  SHOULD_TRANSFORM,
+  SHOULD_POPULATE,
+  SHOULD_TEST,
+} from './config';
+
 const main = async () => {
-  // console.info('Starting to transform raw data');
-  // await transformHandler();
-  // console.info('Sucessfully transformed raw data');
+  if (SHOULD_TRANSFORM) {
+    console.info('Starting to transform raw data');
+    await transformHandler();
+    console.info('Sucessfully transformed raw data');
+  }
 
-  // console.info('Starting to populate database');
-  // await populateHandler();
-  // console.info('Successfully populated database');
+  if (SHOULD_POPULATE) {
+    console.info('Starting to populate database');
+    await populateHandler();
+    console.info('Successfully populated database');
+  }
 
-  console.info('Starting to test the interfaces');
-  await testHandler();
-  console.info('Successfully tested the interfaces');
+  if (SHOULD_TEST) {
+    console.info('Starting to test the interfaces');
+    await testHandler();
+    console.info('Successfully tested the interfaces');
+  }
 };
 
 main();
